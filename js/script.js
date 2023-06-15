7bf10244717e0e5b877f98bfc9c4dc9f04ac2f6d
Breno Acioli <br3noacioli@gmail.com>
2023-06-15 03:30:00 -0300

    Added some comments with some thoughts of how i could modify the grid size

--- js/script.js
+++ js/script.js
@@ -29,7 +29,26 @@ let isMouseDown = false;
             }
         });
     }
-    
+
+//16 quadrados de altura/largura de 35/35 -> largura total 16*35 = 560
+//quadrados*largura(ou altura) = 560
+//largura(ou altura) = 560/quadrados
+//560 será um valor fixo de largura e altura total do grid
+//quero modificar o número de quadrados, porém mantendo a mesma altura e largura total do grid
+//posso então criar uma função que ver quando quadrados XxX o usuário selecionou
+//para colocar no grid e com isso eu modifico a largura e altura dos quadrados
+
+//function gridSize(size){
+	//modificar style square height para 560/size
+	//modificar style square width para 560/size
+//}
+
+//quando eu clicar no botão e selecionar o tamanho da grid "SizexSize", 
+//a função gridSize(size) vai ser chamada e o css .square será modificado
+
+//fazer esse problema primeiro com o prompt pedindo o número de quadrados (com limite de 100)
+//depois removo e adiciono aquele botão que move na lateral 
+
 
   
 
