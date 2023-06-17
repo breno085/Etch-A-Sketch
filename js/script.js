cf4b69903ec508c7c93f650b562f687f1fcbe91e
Breno Acioli <br3noacioli@gmail.com>
2023-06-17 02:52:58 -0300

    Almost done with the project, only missing darkening effect and better front end styling

--- js/script.js
+++ js/script.js
@@ -1,22 +1,82 @@
 const container = document.querySelector('#container');
-const button = document.querySelector('#gridsize');
+const gridButton = document.querySelector('#gridsize');
+const clearButton = document.querySelector('#clear');
+const rainbow = document.querySelector('#rainbow');
+const black = document.querySelector('#black');
+const eraser = document.querySelector('#eraser');
 
-let isMouseDown = false;
 let size = 16;
+let color;
 
-const square = document.querySelector('.square');
+//using a boolean flag to control the behavior of event listeners based on certain conditions or actions,
+//in this case, 3 events when the user uses the mouse: mousedown, mouseup and mousemove, 
+//altering the squares colors based on this 3 conditions
+let isMouseDown = false;
+
+//using a boolean flag again to control the behavior of the default event (black pen) overwriting the rainbow event (rainbow pen),
+//meaning that the squares was still staying black even when the rainbow button was clicked
+let isRainbowEnabled = false;
+
+gridButton.addEventListener('click', () => {
+    gridSize();   
+});
+
+black.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        backcolorChange(square, 'black')
+    });
+})
+
+rainbow.addEventListener('click', () => {
+    isRainbowEnabled = true;
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        let colorNew = rainBow();
+        backcolorChange(square, colorNew);
+    });
+    
+});
+
+eraser.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        backcolorChange(square, 'whitesmoke');
+    });
+});
+
+clearButton.addEventListener('click', () => {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'whitesmoke';
+    });
+});
+
 squareNumber(size);
 squareSize(size);
 
-button.addEventListener('click', () => {
-    gridSize();
-});
+function rainBow(){
+    let r = Math.floor((Math.random() * 255));
+    let g = Math.floor((Math.random() * 255));
+    let b = Math.floor((Math.random() * 255));
+
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+function squareSize(size){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.width = `${560 / size}px`;
+        square.style.height = `${560 / size}px`;
+    });
+}
 
 function gridSize(){
     let sizeNum = prompt('Choose the number of squares', '');
     if (sizeNum !== null && sizeNum > 0 && sizeNum < 101) {
     container.innerHTML = '';
     size = sizeNum;
+    isRainbowEnabled = false;
     squareNumber(size);
     squareSize(size);
     } else {
@@ -33,15 +93,15 @@ function squareNumber(size){
         const square = document.createElement('div');
         square.classList.add('square');
         row.appendChild(square);
-        
-        backcolorChange(square);
+        if (!isRainbowEnabled){
+        backcolorChange(square, 'black');
         }
-        
+        }
         container.appendChild(row);
     }
 }
 
-function backcolorChange(square){
+function backcolorChange(square, color){
     square.addEventListener('mousedown', () => {
         isMouseDown = true;
     });
@@ -50,44 +110,7 @@ function backcolorChange(square){
     });
     square.addEventListener('mousemove', () => {
         if (isMouseDown) {
-            square.style.backgroundColor = 'black';
-        }
-    });
-}
-    
-function squareSize(size){
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => {
-        square.style.width = `${560 / size}px`;
-        square.style.height = `${560 / size}px`;
-    });
-}
-
-//16 quadrados de altura/largura de 35/35 -> largura total 16*35 = 560
-//quadrados*largura(ou altura) = 560
-//largura(ou altura) = 560/quadrados
-//560 será um valor fixo de largura e altura total do grid
-//quero modificar o número de quadrados, porém mantendo a mesma altura e largura total do grid
-//posso então criar uma função que ver quando quadrados XxX o usuário selecionou
-//para colocar no grid e com isso eu modifico a largura e altura dos quadrados
-
-//function gridSize(size){
-	//modificar style square height para 560/size
-	//modificar style square width para 560/size
-//}
-
-//quando eu clicar no botão e selecionar o tamanho da grid "SizexSize", 
-//a função gridSize(size) vai ser chamada e o css .square será modificado
-
-//fazer esse problema primeiro com o prompt pedindo o número de quadrados (com limite de 100)
-//depois removo e adiciono aquele botão que move na lateral 
-
-//Algoritmo
-//1- Adicionar um prompt que executa quando eu aperto o botão
-//2- Fazer uma função rodar ao clicar o botão
-//3- A Função receber o valor digitado no prompt
-//4- A função modifica o número de quadrados da grid
-//5- A função é executada (para valores de 1 a 64 (ou 100))
-
-
-
+            square.style.backgroundColor = `${color}`;
+            }
+        });
+    }
