48539df5fa50255251166f5d37d35ab15c68c457
Breno Acioli <br3noacioli@gmail.com>
2023-06-16 21:26:33 -0300

    New grid generator when a user click the grid size button

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
 const container = document.querySelector('#container');
-let isMouseDown = false;
+const button = document.querySelector('#gridsize');
 
-    for (let j = 0; j < 16; j++){
+let isMouseDown = false;
+let size = 16;
+
+const square = document.querySelector('.square');
+squareNumber(size);
+squareSize(size);
+
+button.addEventListener('click', () => {
+    gridSize();
+});
+
+function gridSize(){
+    let sizeNum = prompt('Choose the number of squares', '');
+    if (sizeNum !== null && sizeNum > 0 && sizeNum < 101) {
+    container.innerHTML = '';
+    size = sizeNum;
+    squareNumber(size);
+    squareSize(size);
+    } else {
+        alert('Choose a valid size between 1 and 100');
+    }
+}
+
+function squareNumber(size){
+    for (let j = 0; j < size; j++){
         const row = document.createElement('div');
         row.classList.add('row');
 
-        for (let i = 0; i < 16; i++){
+        for (let i = 0; i < size; i++){
         const square = document.createElement('div');
         square.classList.add('square');
         row.appendChild(square);
@@ -15,20 +39,29 @@ let isMouseDown = false;
         
         container.appendChild(row);
     }
+}
 
-    function backcolorChange(square){
-        square.addEventListener('mousedown', () => {
-            isMouseDown = true;
-        });
-        square.addEventListener('mouseup', () => {
-            isMouseDown = false;
-        });
-        square.addEventListener('mousemove', () => {
-            if (isMouseDown) {
-                square.style.backgroundColor = 'black';
-            }
-        });
-    }
+function backcolorChange(square){
+    square.addEventListener('mousedown', () => {
+        isMouseDown = true;
+    });
+    square.addEventListener('mouseup', () => {
+        isMouseDown = false;
+    });
+    square.addEventListener('mousemove', () => {
+        if (isMouseDown) {
+            square.style.backgroundColor = 'black';
+        }
+    });
+}
+    
+function squareSize(size){
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.width = `${560 / size}px`;
+        square.style.height = `${560 / size}px`;
+    });
+}
 
 //16 quadrados de altura/largura de 35/35 -> largura total 16*35 = 560
 //quadrados*largura(ou altura) = 560
@@ -49,6 +82,12 @@ let isMouseDown = false;
 //fazer esse problema primeiro com o prompt pedindo o número de quadrados (com limite de 100)
 //depois removo e adiciono aquele botão que move na lateral 
 
+//Algoritmo
+//1- Adicionar um prompt que executa quando eu aperto o botão
+//2- Fazer uma função rodar ao clicar o botão
+//3- A Função receber o valor digitado no prompt
+//4- A função modifica o número de quadrados da grid
+//5- A função é executada (para valores de 1 a 64 (ou 100))
 
-  
+
 
